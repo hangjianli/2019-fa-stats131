d4015854961776fef7b1637cd205e95a365fa3f1
hangjianli <lihangjian123@ucla.edu>
2019-09-30 13:48:19 -0700

    deleted two functions

--- hello_world.py
+++ hello_world.py
@@ -2,13 +2,6 @@ def hello():
     print("Hello World!")
     return("hello")
 
-def hi():
-   print('Hi')
-
-def hello_withnoreturn():
-    print('hello world!')
-
-
 if __name__ == '__main__':
     a = hello()
     print(a)
