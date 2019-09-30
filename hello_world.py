01db696cb49afd5dfb8ff51d8925c4996d782d34
hangjianli <lihangjian123@ucla.edu>
2019-09-30 09:28:59 -0700

    added hello with no return function

--- hello_world.py
+++ hello_world.py
@@ -5,8 +5,13 @@ def hello():
 def hi():
    print('Hi')
 
+def hello_withnoreturn():
+    print('hello world!')
+
+
 if __name__ == '__main__':
     a = hello()
     print(a)
     hi()
+    hello_withnoreturn()
 
