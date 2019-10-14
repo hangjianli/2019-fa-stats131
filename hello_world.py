7cd2de4088d2100645d2fd01390e02594f211c75
hangjianli <lihangjian123@ucla.edu>
2019-10-13 22:07:02 -0700

    added hello no return function

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,16 @@ def hello():
     print("Hello World!")
     return("hello")
 
+def hello_no_return():
+    print('Hello world!')
+
+
 if __name__ == '__main__':
     a = hello()
+    hello_no_return()
     print(a)
-    
+
+
+
+
+
